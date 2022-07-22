eb627f6b5eb1f0ce461c551c5db440375efc3b5b
letsbefriendzz <89048358+letsbefriendzz@users.noreply.github.com>
2022-07-22 06:59:11 -0400

    add efficiency routes

--- lcbo-rank-backend/routes/api.php
+++ lcbo-rank-backend/routes/api.php
@@ -1,22 +1,10 @@
 <?php
 
-use App\Http\Controllers\AlcoholController;
 use App\Http\Controllers\BeerController;
 use App\Http\Controllers\SpiritsController;
 use App\Http\Controllers\WineController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -27,13 +15,14 @@ Route::prefix('alcohol')->group(function () {
         ->prefix('beer')
         ->group(function () {
             Route::get('/', 'getDefault');
+            Route::get('/efficient', 'getEfficient');
         });
 
     Route::controller(SpiritsController::class)
         ->prefix('spirits')
         ->group(function () {
             Route::get('/', 'getDefault');
-
+            Route::get('/efficient', 'getEfficient');
         });
 
     Route::controller(WineController::class)
@@ -43,5 +32,6 @@ Route::prefix('alcohol')->group(function () {
 
         });
 
+    Route::get('/efficient', 'App\Http\Controllers\AlcoholController@getEfficient');
     Route::get('/{alcohol}', 'App\Http\Controllers\AlcoholController@show');
 });
