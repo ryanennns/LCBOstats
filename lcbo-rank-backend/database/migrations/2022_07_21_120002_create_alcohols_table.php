f328f2085f74b58f2d505bb00f1d2e6deb4aeb2a
letsbefriendzz <89048358+letsbefriendzz@users.noreply.github.com>
2022-07-22 06:59:34 -0400

    add price_index

--- lcbo-rank-backend/database/migrations/2022_07_21_120002_create_alcohols_table.php
+++ lcbo-rank-backend/database/migrations/2022_07_21_120002_create_alcohols_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -22,6 +21,7 @@ return new class extends Migration
             $table->double('price');         /*ec_price*/
             $table->double('volume');/*lcbo_unit_volume*/
             $table->double('alcohol_content');  /*lcbo_alcohol_percent*/
+            $table->double('price_index')->default(0.00);
             $table->string('country')->nullable();          /*country_of_manufacture*/
             $table->longText('url')->nullable();              /*clickableuri*/
         });
