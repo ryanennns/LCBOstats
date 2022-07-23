314200b8b61f22b24e73061276d042bfaa39d54f
letsbefriendzz <89048358+letsbefriendzz@users.noreply.github.com>
2022-07-23 11:39:44 -0400

    cleanup

--- lcbo-rank-backend/app/Http/Controllers/SpiritsController.php
+++ lcbo-rank-backend/app/Http/Controllers/SpiritsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Alcohol;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use function MongoDB\BSON\toJSON;
 
 class SpiritsController extends Controller
 {
