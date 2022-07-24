309c02d8f2c3eb47cc3309a5d8e69a895be757d5
letsbefriendzz <89048358+letsbefriendzz@users.noreply.github.com>
2022-07-24 07:02:01 -0400

    add alcohol get by name query

--- lcbo-rank-backend/app/Http/Controllers/AlcoholController.php
+++ lcbo-rank-backend/app/Http/Controllers/AlcoholController.php
@@ -6,14 +6,15 @@ use App\Models\Alcohol;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use Mockery\Exception;
 
 class AlcoholController extends Controller
 {
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Alcohol  $alcohol
-     // * @return \Illuminate\Http\Response
+     * @param \App\Models\Alcohol $alcohol
+     * // * @return \Illuminate\Http\Response
      */
     public function show(Alcohol $alcohol)
     {
@@ -27,4 +28,13 @@ class AlcoholController extends Controller
             ->get()
             ->take(30);
     }
+
+    public function name(Request $request)
+    {
+        if (!$name = $request->input('name', ''))
+            return new Response('', 404);
+        return array_values(Alcohol::get()
+            ->where('title', 'contains', $name)->toArray());
+    }
 }
+
