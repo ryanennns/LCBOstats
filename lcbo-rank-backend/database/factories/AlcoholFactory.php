7fb261161952d61fa12616e13a92153795802c52
letsbefriendzz <89048358+letsbefriendzz@users.noreply.github.com>
2022-08-04 06:20:44 -0400

    fix @extends

--- lcbo-rank-backend/database/factories/AlcoholFactory.php
+++ lcbo-rank-backend/database/factories/AlcoholFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\odel=Alcohol>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Alcohol>
  */
 class AlcoholFactory extends Factory
 {
