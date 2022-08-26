0e106aac0f0bed6e06c68ed98ce4902853c344c3
letsbefriendzz <89048358+letsbefriendzz@users.noreply.github.com>
2022-08-26 11:26:48 -0400

    add cooler seeder:

--- lcbo-rank-backend/database/seeders/CoolerSeeder.php
+++ lcbo-rank-backend/database/seeders/CoolerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Alcohol;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
