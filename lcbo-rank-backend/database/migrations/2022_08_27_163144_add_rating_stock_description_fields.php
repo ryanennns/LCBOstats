dbc0c789f7834673b5512795bc7035371e9be033
Ryan Enns <89048358+letsbefriendzz@users.noreply.github.com>
2022-09-11 12:49:17 -0400

    change 'rating' to double

--- lcbo-rank-backend/database/migrations/2022_08_27_163144_add_rating_stock_description_fields.php
+++ lcbo-rank-backend/database/migrations/2022_08_27_163144_add_rating_stock_description_fields.php
@@ -16,7 +16,7 @@ return new class extends Migration
         // todo make rating a double (fix python script rofl)
         // todo make tests for sorting by rating, filter by out_of_stock
         Schema::table('alcohols', function (Blueprint $table) {
-            $table->string('rating')->nullable()->default(0.0);
+            $table->double('rating')->nullable()->default(0.0);
             $table->string('out_of_stock')->nullable();
             $table->longText('description')->nullable();
         });
