04620c0b735d266ae24d67f7cc0fa2b1cbccd7e2
Ryan Enns <89048358+letsbefriendzz@users.noreply.github.com>
2022-09-26 09:06:54 -0400

    remove /updated

--- lcbo-rank-backend/routes/api.php
+++ lcbo-rank-backend/routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('alcohol')->group(function () {
     Route::get('/efficient', 'App\Http\Controllers\AlcoholController@getEfficient');
     Route::get('/', 'App\Http\Controllers\AlcoholController@getDefault');
-    Route::get('/updated', 'App\Http\Controllers\AlcoholController@updatedStatus');
+//    Route::get('/updated', 'App\Http\Controllers\AlcoholController@updatedStatus');
 });
