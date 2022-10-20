a0590996e82444217301be39dd818bad59ffeaad
Ryan Enns <89048358+letsbefriendzz@users.noreply.github.com>
2022-10-20 15:44:18 -0400

    api.php cleanup

--- lcbo-rank-backend/routes/api.php
+++ lcbo-rank-backend/routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('alcohol')->group(function () {
-    Route::get('/updated', 'App\Http\Controllers\AlcoholController@getUpdated');
-    Route::get('/efficient', 'App\Http\Controllers\AlcoholController@getEfficient');
-    Route::get('/{alcohol}', 'App\Http\Controllers\AlcoholController@show');
-    Route::get('/', 'App\Http\Controllers\AlcoholController@getDefault');
+    Route::get('/updated', 'App\Http\Controllers\AlcoholController@getUpdated')
+        ->name('api.alcohol.updated');
+    Route::get('/{alcohol}', 'App\Http\Controllers\AlcoholController@show')
+        ->name('api.alcohol.show');
+    Route::get('/', 'App\Http\Controllers\AlcoholController@getDefault')
+        ->name('api.alcohol');
 });
