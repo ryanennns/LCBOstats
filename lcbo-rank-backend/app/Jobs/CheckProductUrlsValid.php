05878450d6d74068a0b96b95b6f7fdd7f869bbf6
ryanennns <whycantwebefriendzz@gmail.com>
2023-06-16 16:11:40 -0400

    create job to check if urls return 404 or not

---
+++ lcbo-rank-backend/app/Jobs/CheckProductUrlsValid.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Alcohol;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CheckProductUrlsValid implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Alcohol::query()->chunk(20, function() {
+
+        });
+    }
+}
