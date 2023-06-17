a2cafdb0e4afff873c9262d3eae5b8072137c4b1
ryanennns <whycantwebefriendzz@gmail.com>
2023-06-17 10:11:39 -0400

    snickers

--- lcbo-rank-backend/tink.php
+++ lcbo-rank-backend/tink.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Http;
 
-Http::withHeaders([
+dump(Http::withHeaders([
     'UA' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-])->get('https://www.lcbo.com/en/bud-light-1107')->status();
+])->get('https://www.lcbo.com/en/bud-light-1107')->status());
