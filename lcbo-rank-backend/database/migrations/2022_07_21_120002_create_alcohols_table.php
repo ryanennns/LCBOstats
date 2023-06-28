40dc74161523e536310ad08e3081e20608701178
ryanennns <whycantwebefriendzz@gmail.com>
2023-06-28 09:05:17 -0400

    nuke docblocks

--- lcbo-rank-backend/database/migrations/2022_07_21_120002_create_alcohols_table.php
+++ lcbo-rank-backend/database/migrations/2022_07_21_120002_create_alcohols_table.php
@@ -5,11 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up(): void
     {
         Schema::create('alcohols', function (Blueprint $table) {
@@ -40,11 +35,6 @@ return new class extends Migration {
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down(): void
     {
         Schema::dropIfExists('alcohols');
--- lcbo-rank-backend/database/migrations/2022_12_22_200731_create_price_changes_table.php
+++ lcbo-rank-backend/database/migrations/2022_12_22_200731_create_price_changes_table.php
@@ -5,11 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('price_changes', function (Blueprint $table) {
@@ -23,11 +18,6 @@ return new class extends Migration {
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('price_changes');
