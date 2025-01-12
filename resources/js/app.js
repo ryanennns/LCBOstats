68a17dea14f33207e648071c16e0f66adf92c309
Ryan Enns <ryanennns@gmail.com>
2025-01-12 10:19:57 -0500

    vue/inertia working

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
-import { resolvePageComponent } from 'vite-plugin-pages';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
-    resolve: name => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .mount(el);
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', {eager: true})
+        return pages[`./Pages/${name}.vue`]
     },
-});
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
+            .use(plugin)
+            .mount(el)
+    },
+}).then(r => r)
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+@inertia
+
+<!-- Vite Scripts -->
+@viteReactRefresh
+@vite('resources/js/app.js')
+</body>
+</html>
